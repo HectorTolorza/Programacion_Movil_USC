fab69910b0b0aaff0ec510afe16a889bb9c82db9
HectorTolorza <113229742+HectorTolorza@users.noreply.github.com>
2023-03-24 20:52:22 -0500

    6th commit

--- Taller7/Proyecto009/app/src/main/java/com/example/proyecto009/MainActivity.java
+++ Taller7/Proyecto009/app/src/main/java/com/example/proyecto009/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView iv1;
     private Button b1;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
